262dbd3e895303bf347f8d5403b266f39182efe5
Handsome <pratikbanger05@gmail.com>
2023-05-26 12:20:14 +0530

    minor

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -4,14 +4,13 @@ const Router = Express.Router()
 import authMiddleware from '../Middleware/authMiddleware.js'
 
 // Importing controller
-import { getUser, updateUser, deleteUser, followUser, unFollowUser, fetchAllUser } from '../Controllers/UserController.js'
+import { getUser, updateUser, deleteUser, followUnfollowUser, fetchAllUser } from '../Controllers/UserController.js'
 
 // Routes for authentication
 Router.get('/fetchalluser/', fetchAllUser)
 Router.get('/getuser/:id', authMiddleware, getUser)
 Router.put('/updateuser/:id', authMiddleware, updateUser)
 Router.delete('/deleteuser/:id', authMiddleware, deleteUser)
-Router.put('/follow/:id', authMiddleware, followUser)
-Router.put('/unfollow/:id', authMiddleware, unFollowUser)
+Router.put('/follow/:id', authMiddleware, followUnfollowUser)
 
 export default Router
