6c8f40723ec325752aa415750641f4d1bea143a0
Handsome <pratikbanger05@gmail.com>
2023-06-04 09:32:59 +0530

    reset

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -163,8 +163,8 @@ export const followUnFollowUser = async (req, res) => {
             if (!(_id === id)) {
 
                 if (!followUser.followers.includes(_id)) {
-                    await followUser.updateOne({ $push: { followers: user } })
-                    await user.updateOne({ $push: { following: followUser } })
+                    await followUser.updateOne({ $push: { followers: _id } })
+                    await user.updateOne({ $push: { following: id } })
 
                     const updatedUser = await UserModel.findById(_id)
                     let updatedUserList = updatedUser.following
@@ -174,8 +174,8 @@ export const followUnFollowUser = async (req, res) => {
                     res.status(200).json({ success, message, updatedUserList })
                 }
                 else {
-                    await followUser.updateOne({ $pull: { followers: user } })
-                    await user.updateOne({ $pull: { following: followUser } })
+                    await followUser.updateOne({ $pull: { followers: _id } })
+                    await user.updateOne({ $pull: { following: id } })
 
                     const updatedUser = await UserModel.findById(_id)
                     let updatedUserList = updatedUser.following
